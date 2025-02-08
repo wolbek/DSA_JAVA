eb6f38289fa0443778fdd9e67d77cb2b11300f4c
wolbek <aakashvish22@gmail.com>
2025-02-08 11:12:54 +0530

    Shortest path for directions given

---
+++ Strings/ShortestPath.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class ShortestPath {
+    public static double getShortestPath(String path){
+        int x = 0, y = 0;
+
+        for(int i =0; i<path.length();i++){
+            char direction = path.charAt(i);
+            if(direction == 'N'){
+                ++y;
+            }
+            if(direction == 'E'){
+                ++x;
+            }
+            if(direction == 'S'){
+                --y;
+            }
+            if(direction == 'W'){
+                --x;
+            }
+        }
+        
+        double distance = Math.sqrt(x*x+y*y);
+        return distance;
+    }
+    public static void main(String[] args){
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}
