21eabc1415033e710f547c014f15be19a3277a44
wolbek <aakashvish22@gmail.com>
2025-07-17 23:14:24 +0530

    Preorder traversal

--- Binary_Trees/BinaryTrees.java
+++ Binary_Trees/BinaryTrees.java
@@ -29,11 +29,21 @@ public class BinaryTrees {
 
             return root;
         }
+
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
 
     public static void main(String[] args) {
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1}; // nodes in preorder sequence.
         Node root = BinaryTree.buildTreeFromPreorderNodes(nodes); // buildTree is a static function, so you do not need to create object of the class to call it, you can call it using class.
         System.out.println(root.data); // root is a Node object, that is, a pointer to a value, not the value, so we need to print the value.
+        BinaryTree.preorder(root);
     }
 }
