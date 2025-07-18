1ccef42f3bb35eef955159428c61f6496b5ff19e
wolbek <aakashvish22@gmail.com>
2025-07-18 11:02:34 +0530

    Sum nodes of tree

---
+++ Binary_Trees/SumNodes.java
@@ -0,0 +1,27 @@
+package Binary_Trees;
+import Binary_Trees.BinaryTrees.Node;
+
+public class SumNodes {
+
+    public static int sumNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftSubtreeSum = sumNodes(root.left);
+        int rightSubtreeSum = sumNodes(root.right);
+
+        return leftSubtreeSum + rightSubtreeSum + root.data;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(sumNodes(root));
+    }
+}
