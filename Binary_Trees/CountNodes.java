ac0ea2e82190b5b4893b20df672cf502239aba7f
wolbek <aakashvish22@gmail.com>
2025-07-18 10:57:14 +0530

    Count tree nodes

---
+++ Binary_Trees/CountNodes.java
@@ -0,0 +1,26 @@
+package Binary_Trees;
+import Binary_Trees.BinaryTrees.Node;
+
+public class CountNodes {
+
+    public static int countNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftSubtreeNodes = countNodes(root.left);
+        int rightSubtreeNodes = countNodes(root.right);
+
+        return (leftSubtreeNodes+rightSubtreeNodes)+1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(countNodes(root));
+    }
+}
