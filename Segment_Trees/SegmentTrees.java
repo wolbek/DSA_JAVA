7276995d53c4027252334521a5a4345a717b896f
wolbek <aakashvish22@gmail.com>
2025-07-27 20:20:27 +0530

    Update on Segment Tree

--- Segment_Trees/SegmentTrees.java
+++ Segment_Trees/SegmentTrees.java
@@ -38,6 +38,27 @@ public class SegmentTrees {
         return getSumUtil(0, 0, n-1, qi, qj);
     }
 
+    public static void updateUtil(int i, int si, int sj, int idx, int diff) { // O(log n)
+        if(idx > sj || idx < si) {
+            return;
+        }
+
+        tree[i] += diff;
+        if(si != sj) { // non-leaf
+            int mid = (si + sj)/2;
+            updateUtil(2*i+1, si, mid, idx, diff); // left
+            updateUtil(2*i+2, mid+1, sj, idx, diff); // right
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1, idx, diff); // segment tree updation
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int n = arr.length;
@@ -54,7 +75,12 @@ public class SegmentTrees {
         int qi = 2;
         int qj = 5;
         System.out.print("Sum from index "+qi+" to "+qj+": ");
-        System.out.println(getSum(arr, 2, 5));
+        System.out.println(getSum(arr, 2, 5)); // 18
+
+        // Update value
+        update(arr, 2, 2);
+        System.out.print("Sum from index "+qi+" to "+qj+": ");
+        System.out.println(getSum(arr, 2, 5)); // 17
     }
     
 }
