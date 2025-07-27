944277752824fd09efc68a2e07efa34e13655473
wolbek <aakashvish22@gmail.com>
2025-07-27 16:35:59 +0530

    Query on Segment Tree

--- Segment_Trees/SegmentTrees.java
+++ Segment_Trees/SegmentTrees.java
@@ -20,15 +20,41 @@ public class SegmentTrees {
         return tree[i];
     }
 
+    public static int getSumUtil(int i, int si, int sj, int qi, int qj){
+        if(qj <= si || qi >=sj) { // non-overlapping
+            return 0;
+        } else if(si >= qi && sj <= qj) { // complete overlap
+            return tree[i];
+        } else{ // partial overlap
+            int mid = (si+sj)/2;
+            int left = getSumUtil(2*i+1, si, mid, qi, qj);
+            int right = getSumUtil(2*i+2, mid+1, sj, qi, qj);
+            return left + right;
+        }
+    }
+
+    public static int getSum(int[] arr, int qi, int qj) {
+        int n = arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj);
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int n = arr.length;
         init(n);
         buildST(arr, 0 , 0, n-1);
 
+        // Printing built segment tree
         for(int i=0;i<tree.length;i++){
             System.out.print(tree[i] + " ");
         }
+        System.out.println();
+
+        // Printing sum for given query from index 2 to 5
+        int qi = 2;
+        int qj = 5;
+        System.out.print("Sum from index "+qi+" to "+qj+": ");
+        System.out.println(getSum(arr, 2, 5));
     }
     
 }
