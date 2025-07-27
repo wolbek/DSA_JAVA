573341b7708fb263d8ad6f69999237b223799626
wolbek <aakashvish22@gmail.com>
2025-07-27 22:16:35 +0530

    Max Element Query: Update Element

--- Segment_Trees/MaxQuery.java
+++ Segment_Trees/MaxQuery.java
@@ -7,7 +7,7 @@ public class MaxQuery {
         tree = new int[4*n];
     }
 
-    public static void buildTree(int i, int si, int sj, int[] arr){
+    public static void buildTree(int i, int si, int sj, int[] arr){ // O(n)
         if(si == sj){
             tree[i] = arr[si];
             return;
@@ -25,7 +25,7 @@ public class MaxQuery {
         return getMaxUtil(0,0,n-1,qi,qj);
     }
 
-    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) { // O(log n)
         if(si > qj || sj < qi) { // no overlap
             return Integer.MIN_VALUE;
         } else if(si >= qi && sj <= qj) { // complete overlap
@@ -38,6 +38,28 @@ public class MaxQuery {
         }
     }
 
+    public static void update(int[] arr, int idx, int newVal){
+        arr[idx] = newVal;
+        int n = arr.length;
+        updateUtil(0, 0, n-1, idx, newVal);
+    }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int newVal) { // O(log n)
+        if(idx < si || idx > sj) {
+            return;
+        }
+
+        if(si == sj){
+            tree[i] = newVal;
+        }
+
+        if(si != sj) { // Doing this only for non leaves
+            tree[i] = Math.max(tree[i], newVal);
+            int mid = (si+sj)/2;
+            updateUtil(2*i+1, si, mid, idx, newVal); // left
+            updateUtil(2*i+2, mid+1, sj, idx, newVal); // right
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {6,8,-1,2,17,1,3,2,4};
         int n = arr.length;
@@ -53,6 +75,13 @@ public class MaxQuery {
         int qj = 5;
         System.out.print("Max from index " + qi + " to " + qj+": ");
         int max = getMax(arr, qi, qj);
-        System.out.println(max);
+        System.out.println(max); // 17
+
+        // Update
+        update(arr, 2, 20);
+
+        System.out.print("Max from index " + qi + " to " + qj+": ");
+        max = getMax(arr, qi, qj);
+        System.out.println(max); // 20
     }
 }
