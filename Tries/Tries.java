76aa8846335932c1518b98733748ff9d159011c6
wolbek <aakashvish22@gmail.com>
2025-07-29 00:14:35 +0530

    Tries: Node class creation

---
+++ Tries/Tries.java
@@ -0,0 +1,25 @@
+package Tries;
+
+public class Tries {
+
+    static class Node{
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void main(String[] args){
+        String[] words = {"the", "a", "there", "their", "any", "thee"};
+    }
+}
+
+
+
+
