1ff11cbd630b7b8161d71fc41c2c5f4ce8fa6a44
wolbek <aakashvish22@gmail.com>
2025-07-29 00:58:34 +0530

    Tries: Insert words

--- Tries/Tries.java
+++ Tries/Tries.java
@@ -15,8 +15,25 @@ public class Tries {
 
     public static Node root = new Node();
 
+    public static void insert(String word) { // O(L)
+        Node curr = root;
+        for(int level=0;level<word.length();level++){
+            int i = word.charAt(level) - 'a';
+            if(curr.children[i] == null) {
+                curr.children[i] = new Node();
+            }
+            curr = curr.children[i];
+        }
+
+        curr.eow = true;
+    }
+
     public static void main(String[] args){
         String[] words = {"the", "a", "there", "their", "any", "thee"};
+
+        for(int i =0;i<words.length;i++){
+            insert(words[i]);
+        }
     }
 }
 
