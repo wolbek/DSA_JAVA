4d7b9bca5b60ee152e89ea0b3b360d5b52b9f318
wolbek <aakashvish22@gmail.com>
2025-08-03 20:22:17 +0530

    Using instance variable root rather than static root for trie

--- Tries/Tries.java
+++ Tries/Tries.java
@@ -13,9 +13,7 @@ public class Tries {
         }
     }
 
-    public static Node root = new Node();
-
-    public static void insert(String word) { // O(L)
+    public static void insert(Node root, String word) { // O(L)
         Node curr = root;
         for(int i=0; i<word.length(); i++){
             int idx = word.charAt(i) - 'a';
@@ -28,7 +26,7 @@ public class Tries {
         curr.eow = true;
     }
 
-    public static boolean search(String key){ // O(L)
+    public static boolean search(Node root, String key){ // O(L)
         Node curr = root;
         for(int i=0; i<key.length(); i++){
             int idx = key.charAt(i) - 'a';
@@ -44,16 +42,18 @@ public class Tries {
     public static void main(String[] args){
         String[] words = {"the", "a", "there", "their", "any", "thee"};
 
+        Node root = new Node();
+
         // Insert
         for(int i =0;i<words.length;i++){
-            insert(words[i]);
+            insert(root, words[i]);
         }
 
         // Search
-        System.out.println(search("thee"));
-        System.out.println(search("thor"));
-        System.out.println(search("any"));
-        System.out.println(search("an"));
+        System.out.println(search(root, "thee"));
+        System.out.println(search(root, "thor"));
+        System.out.println(search(root, "any"));
+        System.out.println(search(root, "an"));
     }
 }
 
