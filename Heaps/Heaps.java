9de3e4f7be78dfc75b5444c41f53debf3910f01b
wolbek <aakashvish22@gmail.com>
2025-08-04 16:26:43 +0530

    Heap: Insert element in heap

---
+++ Heaps/Heaps.java
@@ -0,0 +1,36 @@
+package Heaps;
+
+import java.util.ArrayList;
+
+public class Heaps {
+    static class Heap{
+        // Implementing min heap
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data){
+            arr.add(data);
+
+            int cIdx = arr.size()-1; // child index
+            int pIdx = (cIdx - 1)/2; // parent index
+
+            while(arr.get(cIdx) < arr.get(pIdx)){ // O(log n)
+                // swap
+                int temp = arr.get(cIdx);
+                arr.set(cIdx, arr.get(pIdx));
+                arr.set(pIdx, temp);
+
+                cIdx = pIdx;
+                pIdx = (cIdx-1)/2;
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+    }
+}
