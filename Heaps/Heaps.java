105daa44f2e1e268b02dde234470879d6bed2450
wolbek <aakashvish22@gmail.com>
2025-08-04 20:32:57 +0530

    Heaps: Remove element

--- Heaps/Heaps.java
+++ Heaps/Heaps.java
@@ -22,13 +22,57 @@ public class Heaps {
                 cIdx = pIdx;
                 pIdx = (cIdx-1)/2;
             }
-
         }
 
         public int peek(){
             return arr.get(0);
         }
 
+        private void heapify(int i) {
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(left) < arr.get(minIdx)){
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(right) < arr.get(minIdx)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                // Now it can happen that the below heap is unfixed now
+                // So we need to continue swapping top to bottom, to get the biggest element at bottom.
+
+                heapify(minIdx);
+            }
+        } 
+
+        public int remove(){
+            int data = arr.get(0);
+
+            // Step 1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            // Step 2 - delete last
+            arr.remove(arr.size()-1);
+
+            // Step 3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
     }
 
     public static void main(String[] args){
@@ -38,6 +82,9 @@ public class Heaps {
         h.add(1);
         h.add(5);
 
-        System.out.println(h.peek());
+        while(!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
