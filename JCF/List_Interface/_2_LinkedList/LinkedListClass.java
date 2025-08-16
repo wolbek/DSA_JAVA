ffab1b3844339af79eed8b6d4f37dd222e1d4b76
wolbek <aakashvish22@gmail.com>
2025-08-16 16:10:23 +0530

    LinkedList: addFirst(), addLast(), add(), print()

---
+++ JCF/List_Interface/_2_LinkedList/LinkedListClass.java
@@ -0,0 +1,88 @@
+package JCF.List_Interface._2_LinkedList;
+
+public class LinkedListClass {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+        } else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+        } else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void add(int idx, int data) {
+        if(idx == 0){
+            addFirst(data);
+        } else{
+            Node newNode = new Node(data);
+            Node temp = head;
+            int i = 0;
+
+            while(i!=idx-1){
+                temp = temp.next;
+                i++;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    public void print() {
+        if(head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp!=null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedListClass ll = new LinkedListClass();
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+        // For add we're considering they'll pass proper index within valid range.
+        ll.add(2,9);
+        ll.print();
+
+    }
+}
