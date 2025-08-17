264e9cab694f3f29ec48a820e24f3c61ee607c4e
wolbek <aakashvish22@gmail.com>
2025-08-17 19:53:17 +0530

    DoublyLinkedList: Reverse Doubly LL

--- JCF/List_Interface/_2_LinkedList/DoublyLL.java
+++ JCF/List_Interface/_2_LinkedList/DoublyLL.java
@@ -62,18 +62,44 @@ public class DoublyLL {
         return val;
     }
 
+    public void reverse() {
+        Node prevElem = null;
+        Node currElem = null;
+        Node nextElem = head;
+        tail = head;
+
+        while(nextElem != null){
+            prevElem = currElem;
+            currElem = nextElem;
+            nextElem = nextElem.next;
+
+            currElem.next = prevElem;
+            currElem.prev = nextElem;
+        }
+
+        head = currElem;
+    }
 
     public static void main(String[] args) {
         DoublyLL dll = new DoublyLL();
+
+        // Add First
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
 
+        // Print
         dll.print();
         System.out.println(dll.size);
 
+        // Remove First
         dll.removeFirst();
         dll.print();
         System.out.println(dll.size);
+
+        // Reverse
+        System.out.println("Reverse:");
+        dll.reverse();
+        dll.print();
     }
 }
