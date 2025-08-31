83c176c2da5121e90ae35e7bd863c3e7ea05bf58
wolbek <aakashvish22@gmail.com>
2025-08-31 19:41:50 +0530

    DP: Catalan - Tabulation

---
+++ Dynamic_Programming/CatalanTabulation.java
@@ -0,0 +1,21 @@
+package Dynamic_Programming;
+
+public class CatalanTabulation {
+    public static int catalan(int n) { // TC: O(n*n)
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) { // Ci
+            for(int j=0; j<i; j++) {
+                dp[i] += dp[j] * dp[i-1-j]; // Ci = Cj * Ci-1-j
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(catalan(4));
+    }
+}
