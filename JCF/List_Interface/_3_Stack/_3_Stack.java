8bc2c7d907006eb68e62ae08c0ddf83230c3b48c
wolbek <aakashvish22@gmail.com>
2025-09-08 10:43:44 +0530

    Stack: Implementation using JCF

--- JCF/List_Interface/_3_Stack/_3_Stack.java
+++ JCF/List_Interface/_3_Stack/_3_Stack.java
@@ -26,6 +26,9 @@ public class _3_Stack {
         //Creation
         Stack<Integer> s=new Stack<>();//stack functions are different, push() is not defined for List<Integer> but it's defined for Stack<Integer>
         
+        //Clear the stack
+        s.clear();
+
         //Add at top
         s.push(10);
         s.push(20);
@@ -57,10 +60,5 @@ public class _3_Stack {
 
         //Get element at index
         System.out.println("Element at index 2"+s.get(2));
-
-        //One of the ways to clear stack
-        s = new Stack<>();
-        System.out.println("Cleared the stack: "+s);
-        
     }
 }
