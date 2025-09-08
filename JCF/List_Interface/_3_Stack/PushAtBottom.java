68a0d0544e60a9a28c184850dfde7454f3847b19
wolbek <aakashvish22@gmail.com>
2025-09-08 11:10:36 +0530

    Stack: Push at bottom

---
+++ JCF/List_Interface/_3_Stack/PushAtBottom.java
@@ -0,0 +1,28 @@
+package JCF.List_Interface._3_Stack;
+
+import java.util.Stack;
+
+public class PushAtBottom {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s, 0);
+
+        System.out.println(s);
+    }
+}
